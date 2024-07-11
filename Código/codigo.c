a12f3fe4612f83a7f4c01da712f7cd911551b3fd
decker12w <josemaia.comp@gmail.com>
2024-07-11 13:49:59 -0300

    refactor

--- Código/codigo.c
+++ Código/codigo.c
@@ -22,14 +22,18 @@ int cont_pessoa = 0;
 // Variáveis globais de barreiras, threads e mutex
 pthread_barrier_t pessoasBarco;
 pthread_mutex_t mutex;
+pthread_mutex_t boat_mutex;
 sem_t hacker_queue;
 sem_t serf_queue;
 
 void *routine(void *args)
 {
+    // José
+    sleep(rand() % 5); // sleep para simular a chegada de pessoas (hackers e serfs)
     int index = *(int *)args;
     bool isCaptain = false;
 
+    // Taichi
     pthread_mutex_lock(&mutex);
     if (index == 0) // caso hackers entrarem no barco
     {
@@ -86,15 +90,20 @@ void *routine(void *args)
         }
     }
 
+    // José
     if (index == 0) // faz o semáforo esperar o valor de um semáforo (hacker)
     {
         sem_wait(&hacker_queue);
+        pthread_mutex_lock(&boat_mutex);
         hackers_on_boat++;
+        pthread_mutex_unlock(&boat_mutex);
     }
     else // faz o semáforo esperar o valor de um semáforo (serf)
     {
         sem_wait(&serf_queue);
+        pthread_mutex_lock(&boat_mutex);
         serfs_on_boat++;
+        pthread_mutex_unlock(&boat_mutex);
     }
 
     board(index);
@@ -117,51 +126,38 @@ void *routine(void *args)
 
 int main()
 {
-    // Iniciando as threads e as barreiras
+    // José
     pthread_t th[ThreadsMAX];
     pthread_mutex_init(&mutex, NULL);
+    pthread_mutex_init(&boat_mutex, NULL);
     pthread_barrier_init(&pessoasBarco, NULL, boatNumber);
     sem_init(&hacker_queue, 0, 0);
     sem_init(&serf_queue, 0, 0);
 
-    int pessoas[ThreadsMAX]; // possibilidades máximas de pessoas 16
-    int opc;
-    do
+    int pessoas[ThreadsMAX];
+    int num_hackers, num_serfs;
+
+    printf("Digite a quantidade de hackers: ");
+    scanf("%d", &num_hackers);
+    printf("Digite a quantidade de serfs: ");
+    scanf("%d", &num_serfs);
+
+    if (num_hackers + num_serfs > ThreadsMAX)
     {
-        // Menu de opções
-        printf("Escolha entre as opções\n");
-        printf("1 - Adicionar hacker\n");
-        printf("2 - Adicionar serf\n");
-        printf("3 - Executar programa\n");
-        scanf("%d", &opc);
+        printf("Erro: A soma de hackers e serfs excede o limite máximo de %d.\n", ThreadsMAX);
+        return 1;
+    }
 
-        if (cont_pessoa == ThreadsMAX) // caso atingir o número máximo
-        {
-            printf("Desculpe o número máxima de pessoas foi atingida\n");
-            printf("Iremos executar o código\n");
-            continue;
-        }
-        switch (opc)
-        {
-        case 1: // adicionando hacker
-            pessoas[cont_pessoa] = 0;
-            cont_pessoa++;
-            break;
-        case 2: // adicionando serf
-            pessoas[cont_pessoa] = 1;
-            cont_pessoa++;
-            break;
-        case 3: // executando o programa
-            break;
-        default: // opc inválida
-            printf("Número inválido\nDigite outro\n");
-            scanf("%d", &opc);
-            break;
-        }
-        printf("\n\n\n");
-    } while (opc != 3);
+    for (int i = 0; i < num_hackers; i++, cont_pessoa++)
+    {
+        pessoas[cont_pessoa] = 0; // 0 representa hacker
+    }
 
-    // Executando a rotina nas threads
+    for (int i = 0; i < num_serfs; i++, cont_pessoa++)
+    {
+        pessoas[cont_pessoa] = 1; // 1 representa serf
+    }
+
     for (int i = 0; i < cont_pessoa; i++)
     {
         int *a = malloc(sizeof(int));
@@ -172,7 +168,6 @@ int main()
         }
     }
 
-    // Esperando todas as threads terminarem sua execução
     for (int i = 0; i < cont_pessoa; i++)
     {
         if (pthread_join(th[i], NULL) != 0)
@@ -181,8 +176,8 @@ int main()
         }
     }
 
-    // Destruindo os arquivos resíduais e terminando o programa
     pthread_mutex_destroy(&mutex);
+    pthread_mutex_destroy(&boat_mutex);
     pthread_barrier_destroy(&pessoasBarco);
     sem_destroy(&hacker_queue);
     sem_destroy(&serf_queue);
@@ -198,4 +193,4 @@ void board(int arg)
 void rowboard(int arg)
 {
     printf("Remando %s\n", arg == 0 ? "hackers" : "serfs");
-};
+}
