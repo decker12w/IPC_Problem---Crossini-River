a352e452314d93760a6df6dbffc81007434cdde6
decker12w <josemaia.comp@gmail.com>
2024-07-18 20:16:27 -0300

    refactor

--- Código/codigo.c
+++ Código/codigo.c
@@ -22,31 +22,33 @@ int cont_pessoa = 0;
 // Variáveis globais de barreiras, threads e mutex
 pthread_mutex_t mutex;
 pthread_mutex_t boat_mutex;
-pthread_cond_t barrier_cond;
-int barrier_count = 0;
+pthread_barrier_t barrier;
 sem_t hacker_queue;
 sem_t serf_queue;
 
 void *routine(void *args)
 {
-    sleep(rand() % 5); // sleep para simular a chegada de pessoas (hackers e serfs)
+    // sleep para simular a chegada de pessoas (hackers e serfs)
+    sleep(rand() % 1);
+    // Define se é hacker (0) ou serf (1)
     int index = *(int *)args;
     bool isCaptain = false;
 
+    // Mutex para proteger a seção crítica onde hackers e serfs são contados
     pthread_mutex_lock(&mutex);
-    if (index == 0) // caso hackers entrarem no barco
+    if (index == 0) // Caso hackers entrarem no barco
     {
         hackers++;
-        if (hackers == 4)
+        if (hackers == 4) // Se há 4 hackers, eles formam um grupo e entram no barco
         {
             sem_post(&hacker_queue);
             sem_post(&hacker_queue);
             sem_post(&hacker_queue);
             sem_post(&hacker_queue);
             hackers -= 4;
-            isCaptain = true;
+            isCaptain = true; // um hacker é designado como capitão
         }
-        else if (hackers == 2 && serfs >= 2)
+        else if (hackers == 2 && serfs >= 2) // Se há 2 hackers ou 2 serfs, eles formam um grupo e podem entrar no barco
         {
             sem_post(&hacker_queue);
             sem_post(&hacker_queue);
@@ -54,26 +56,26 @@ void *routine(void *args)
             sem_post(&serf_queue);
             hackers -= 2;
             serfs -= 2;
-            isCaptain = true;
+            isCaptain = true; // Um hacker ou serf é designado como capitão
         }
         else
         {
-            pthread_mutex_unlock(&mutex);
+            pthread_mutex_unlock(&mutex); // Libera o mutex se não formou o grupo
         }
     }
     else
     { // caso serfs entrarem no barco
         serfs++;
-        if (serfs == 4)
+        if (serfs == 4) // Se há 4 serfs, formam um grupo e entram no barco
         {
             sem_post(&serf_queue);
             sem_post(&serf_queue);
             sem_post(&serf_queue);
             sem_post(&serf_queue);
             serfs -= 4;
-            isCaptain = true;
+            isCaptain = true; // Um serf é capitão
         }
-        else if (hackers >= 2 && serfs >= 2)
+        else if (hackers >= 2 && serfs >= 2) // Se há 2 hackers ou 2 serfs, eles formam um grupo e podem entrar no barco
         {
             sem_post(&hacker_queue);
             sem_post(&hacker_queue);
@@ -81,14 +83,15 @@ void *routine(void *args)
             sem_post(&serf_queue);
             hackers -= 2;
             serfs -= 2;
-            isCaptain = true;
+            isCaptain = true; // Ou um hacker ou um serf é capitão
         }
         else
         {
-            pthread_mutex_unlock(&mutex);
+            pthread_mutex_unlock(&mutex); // Libera o mutex se não formou o grupo
         }
     }
 
+    // Hacker ou serf espera até que seu semáforo seja liberado
     if (index == 0) // faz o semáforo esperar o valor de um semáforo (hacker)
     {
         sem_wait(&hacker_queue);
@@ -104,21 +107,11 @@ void *routine(void *args)
         pthread_mutex_unlock(&boat_mutex);
     }
 
+    // Função board quando entra no barco
     board(index);
 
-    // Implementação da barreira usando variável de condição e mutex
-    pthread_mutex_lock(&boat_mutex);
-    barrier_count++;
-    if (barrier_count == boatNumber)
-    {
-        barrier_count = 0;
-        pthread_cond_broadcast(&barrier_cond);
-    }
-    else
-    {
-        pthread_cond_wait(&barrier_cond, &boat_mutex);
-    }
-    pthread_mutex_unlock(&boat_mutex);
+    // Implementação da barreira usando pthread_barrier_wait
+    pthread_barrier_wait(&barrier);
 
     if (isCaptain)
     {
@@ -126,8 +119,8 @@ void *routine(void *args)
         printf("Serfs no barco: %d\n", serfs_on_boat);
         hackers_on_boat = 0;
         serfs_on_boat = 0;
-        rowboard(index);
-        pthread_mutex_unlock(&mutex);
+        rowboard(index);              // Capitão chama a função para remar
+        pthread_mutex_unlock(&mutex); // Capitão libera o mutex para permitir novos grupos
     }
 
     free(args);
@@ -136,27 +129,33 @@ void *routine(void *args)
 
 int main()
 {
+    // Variáveis
     pthread_t th[ThreadsMAX];
     pthread_mutex_init(&mutex, NULL);
     pthread_mutex_init(&boat_mutex, NULL);
-    pthread_cond_init(&barrier_cond, NULL);
+    pthread_barrier_init(&barrier, NULL, boatNumber);
     sem_init(&hacker_queue, 0, 0);
     sem_init(&serf_queue, 0, 0);
 
     int pessoas[ThreadsMAX];
     int num_hackers, num_serfs;
 
+    srand(getpid());
+
+    // Escolha de quantidade de hacker e serfs
     printf("Digite a quantidade de hackers: ");
     scanf("%d", &num_hackers);
     printf("Digite a quantidade de serfs: ");
     scanf("%d", &num_serfs);
 
+    // Caso estoure o limite máximo
     if (num_hackers + num_serfs > ThreadsMAX)
     {
         printf("Erro: A soma de hackers e serfs excede o limite máximo de %d.\n", ThreadsMAX);
         return 1;
     }
 
+    // Inicia o array com hackers ou serfs
     for (int i = 0; i < num_hackers; i++, cont_pessoa++)
     {
         pessoas[cont_pessoa] = 0; // 0 representa hacker
@@ -167,6 +166,7 @@ int main()
         pessoas[cont_pessoa] = 1; // 1 representa serf
     }
 
+    // Criação de threads
     for (int i = 0; i < cont_pessoa; i++)
     {
         int *a = malloc(sizeof(int));
@@ -177,6 +177,7 @@ int main()
         }
     }
 
+    // Espera todas as threads terminarem
     for (int i = 0; i < cont_pessoa; i++)
     {
         if (pthread_join(th[i], NULL) != 0)
@@ -185,11 +186,13 @@ int main()
         }
     }
 
+    // Destrói mutex, barreira e semáforos
     pthread_mutex_destroy(&mutex);
     pthread_mutex_destroy(&boat_mutex);
-    pthread_cond_destroy(&barrier_cond);
+    pthread_barrier_destroy(&barrier);
     sem_destroy(&hacker_queue);
     sem_destroy(&serf_queue);
+    pthread_barrier_init(&barrier, NULL, boatNumber);
     return 0;
 }
 
